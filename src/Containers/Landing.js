5a1556b556c729795594157e33f77dbacd45591f
brianwlego <legobw@gmail.com>
2021-01-07 23:54:41 -0500

    about to merge

--- src/Containers/Landing.js
+++ src/Containers/Landing.js
@@ -5,11 +5,11 @@ import SignUp from '../Components/SignUp'
 function Landing(){
   const [signup, setSignUp] = useState(false)
 
-
   return(
     <div>
       {!signup ? <Login /> : null}
       <button hidden={signup} onClick={()=>setSignUp(true)}>Click To Sign Up</button>
+      {/* SIGNUP SHOULD EVENTUALLY BE A MODAL */}
       {signup ? <SignUp /> : null}
     </div>
   )
